596a9f1ded287b9f05a38fd0007ff93b6b4dc4c9
malloc <malloc_3141592@naver.com>
2024-05-26 22:56:08 +0900

    temporary commit

---
+++ client/src/pages/customcalendar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const CustomCalendar = () => {
+  const navigate = useNavigate();
+
+  return <div></div>;
+};
+export default CustomCalendar;
